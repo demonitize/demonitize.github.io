766a51443d9ec87fd17023ba70e25a9ddf8a19b6
DEMONITIZED BOI <42183865+demonitize@users.noreply.github.com>
2022-10-03 11:59:23 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+//NO IT DOESN'T YOU LIER
+console.log("I have no life");
+
+//captcha shit
+
+/*
+      function add() {
+        console.log('grecaptcha.getResponse')
+        window.location = "https://discordapp.com/api/oauth2/authorize?client_id=551194918853410817&permissions=8&redirect_uri=https%3A%2F%2Fdemonitized-bot-website.glitch.me%2Fsuccess.html&response_type=code&scope=bot%20identify%20guilds"
+      }
+      
+      function errorEXP() {
+        grecaptcha.reset()
+        alert("Your ReCaptcha has expired. Please submit a new ReCaptcha")
+      }
+*/
+
+
+//cookies
+
